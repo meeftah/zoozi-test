54d37963c7a213f34a9a0fc89b084daa0d19e660
meeftah <miftah.yuda@gmail.com>
2025-06-03 18:21:13 +0700

    add entity for user

---
+++ lib/features/authentication/domain/entities/user.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String accessToken;
+
+  const User({
+    required this.accessToken,
+  });
+
+  @override
+  List<Object?> get props => [accessToken];
+}
