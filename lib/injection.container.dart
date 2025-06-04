b49d332c015a6495e56543de3d1f8fe2cb397c5e
meeftah <miftah.yuda@gmail.com>
2025-06-04 08:11:34 +0700

    add DI

---
+++ lib/injection.container.dart
@@ -0,0 +1,63 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:zoozitest/core/network/dio.client.dart';
+import 'package:zoozitest/core/network/network.info.dart';
+import 'package:zoozitest/core/storage/secure.storage.dart';
+import 'package:zoozitest/features/authentication/data/datasources/auth.remote.data.source.dart';
+import 'package:zoozitest/features/authentication/data/repositories/auth.repository.impl.dart';
+import 'package:zoozitest/features/authentication/domain/repositories/auth.repository.dart';
+import 'package:zoozitest/features/authentication/domain/usecases/login.usecase.dart';
+import 'package:zoozitest/features/authentication/domain/usecases/logout.usecase.dart';
+import 'package:zoozitest/features/authentication/domain/usecases/register.usecase.dart';
+import 'package:zoozitest/features/authentication/presentation/bloc/auth.bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features
+  await _initAuth();
+  
+  //! Core
+  await _initCore();
+  
+  //! External
+  await _initExternal();
+}
+
+Future<void> _initAuth() async {
+  // Bloc
+  sl.registerFactory(() => AuthBloc(
+    loginUseCase: sl(),
+    registerUseCase: sl(),
+    logoutUseCase: sl(),
+  ));
+
+  // Use cases
+  sl.registerLazySingleton(() => LoginUseCase(sl()));
+  sl.registerLazySingleton(() => RegisterUseCase(sl()));
+  sl.registerLazySingleton(() => LogoutUseCase(sl()));
+
+  // Repository
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      remoteDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(dioClient: sl()),
+  );
+}
+
+Future<void> _initCore() async {
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+  sl.registerLazySingleton(() => SecureStorage());
+}
+
+Future<void> _initExternal() async {
+  sl.registerLazySingleton(() => DioClient(sl()));
+  sl.registerLazySingleton(() => Connectivity());
+}
